9dfdd7981dfd7dc2294dba2b118da00cad85095f
Vilith <vilith44@gmail.com>
2025-06-17 11:35:40 +0200

    Add project files.

---
+++ Companies.API/DTOs/CompanyDTO.cs
@@ -0,0 +1,13 @@
+﻿using Companies.API.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Companies.API.DTOs
+{
+    public record CompanyDTO
+    {
+        public int Id { get; init; }               
+        public string? Name { get; init; }
+        public string? Address { get; init; }
+        public string? Country { get; init; }
+    }
+}
